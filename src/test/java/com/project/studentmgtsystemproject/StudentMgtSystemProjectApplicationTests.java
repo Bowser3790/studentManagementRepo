640b203fc216acf4f85955471b09fcee0445687e
chrisbowser <bowser3790@gmail.com>
2023-06-06 12:15:26 -0400

    Version 1

---
+++ src/test/java/com/project/studentmgtsystemproject/StudentMgtSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.studentmgtsystemproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMgtSystemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
